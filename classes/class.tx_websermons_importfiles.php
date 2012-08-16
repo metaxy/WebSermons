78debc253e93da738a4d7831be0a63027d8eaa3d
Paul Walger <metaxy@walger.name>
2012-08-16 22:12:21 +0200

    import id3

--- classes/class.tx_websermons_importfiles.php
+++ classes/class.tx_websermons_importfiles.php
@@ -30,6 +30,10 @@
  * @subpackage		tx_smscheddemo
  *
  */
+ 
+if (t3lib_extMgm::isLoaded('t3getid3')) require_once(t3lib_extMgm::extPath('t3getid3').'getid3/getid3.php');
+else die('GetID3() Library not loaded!');
+
 class tx_websermons_ImportFiles extends tx_scheduler_Task {
 
 	/**
@@ -37,8 +41,8 @@ class tx_websermons_ImportFiles extends tx_scheduler_Task {
 	 *
 	 * @var	int		$pageid
 	 */
-	 var $pageid;
-
+    var $pageid;
+    var $monitoredPath;
 	/**
 	 * Function executed from the Scheduler.
 	 * Hides all content elements of a page
@@ -46,40 +50,70 @@ class tx_websermons_ImportFiles extends tx_scheduler_Task {
 	 * @return	boolean	TRUE if success, otherwise FALSE
 	 */
 	public function execute() {
-		$success = FALSE;
-		/*$data = array();
+        $success = true;
+        $monitoredPath = "/srv/www/htdocs/t4/fileadmin/predigten/";
+        $getID3 = t3lib_div::makeInstance('getID3');
+        $notUsed = $this->getNotUsedFiles($monitoredPath);
+        $i = 0;
+        foreach($notUsed as $mp3file) {
+            $i++;
+            if($i > 2)
+                break;
+            $id3Info = $getID3->analyze($mp3file);
+            if (isset($id3Info->info['error'])) {
+                $GLOBALS['BE_USER']->simplelog("could not get id3 from $mp3file", "websermons", 1);
+                $success = false;
+            } else {
+                $dataArr = array();
+                $fieldList = "path,title,speaker,topic,links,parentfolder,pdate";
+                $dataArr["path"] = $mp3file;
+                
+                $dataArr["title"] = "Predigt";
+                $dataArr["speaker"] = $id3Info['tags']['id3v2']['artist'][0];
+                $dataArr["topic"] = $id3Info['tags']['id3v2']['title'][0];
+                $dataArr["links"] = $id3Info['tags']['id3v2']['comments'][0];
+                $dataArr["pdate"] = strtotime($id3Info['tags']['id3v2']['date'][0]);
+                $dataArr["parentfolder"] = 4;
 
-		if (!empty($this->pageid)) {
-			$this->pageid = intval($this->pageid);
+                tslib_cObj::DBgetInsert("tx_websermons_files", 74, $dataArr, $fieldList, true);
+                $GLOBALS['BE_USER']->simplelog(print_r($id3Info, true), "websermons", 1);
+                $GLOBALS['BE_USER']->simplelog("speaker = " . print_r($id3Info['tags']['id3v2']['artist'], true), "websermons", 1);
 
-				// Get uids of all content elements of the page
-			$row = $GLOBALS['TYPO3_DB']->exec_SELECTgetRows(
-				'uid',
-				'tt_content',
-				'pid=' . $this->pageid . t3lib_Befunc::BEenableFields('tt_content') . t3lib_Befunc::deleteClause('tt_content')
-			);
+            }
+            //$GLOBALS['BE_USER']->simplelog("$mp3file pharsed", "websermons");
+        }
+        return $success;
+    }
+    private function getNotUsedFiles($dir)
+    {
+        $fileList = array();
+        if ($handle = opendir($dir)) {
+            $cache = "";
+            $folderList = array();
 
-				// Hide content of page
-			if (!empty($row)) {
-				$tce = t3lib_div::makeInstance('t3lib_TCEmain');
-				foreach ($row as $field) {
-					$data['tt_content'][$field['uid']] = array(
-						'hidden' => '1'
-					);
-				}
-				$tce->start($data,array());
-				$tce->process_datamap();
-
-					// Clear cache of page
-				$tce->clear_cacheCmd($this->pageid);
-			}	
-			$success = TRUE;
-		} else {
-				// No pageid defined, just log the task
-			t3lib_div::devLog('[scheduler: Hide content]: Fail. No page id was given', 'websermons', 3);
-		}*/
-		return $success;
-	}
+            while (false !== ($file = @readdir ($handle))) {
+                if ($file != "." && $file != ".." && $file != "index.php" && $file != "index.html") {
+                    if(!is_dir('../'.$dir.'/'.$file)) {
+                        //todo: sql injection?
+                        $path = $GLOBALS['TYPO3_DB']->fullQuoteStr($dir.'/'.$file);
+                        $count = $GLOBALS['TYPO3_DB']->exec_SELECTcountRows('uid', 'tx_websermons_files', "path = ".$path."");
+                        $vfile = $dir . '/' . $file;
+                        
+                        if($count == 0) {
+                            $fileList[] = $vfile;
+                        }
+                    } else {
+                        $folderList[] = $file;
+                    }
+                }
+            }
+            foreach ($folderList as $value) {
+                $fileList += $this->getNotUsedFiles($dir."/".$value);
+            }           
+        }
+        return $fileList;
+    }
+	
 
 }
 
